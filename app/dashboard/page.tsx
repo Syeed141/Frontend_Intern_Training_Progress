5bc154002fc64b09abd7727dd0936a7ae37ac6c9
Syeed141 <shahariarsyeed2026@gmail.com>
2026-05-13 12:42:24 +0600

    Updated dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../redux/features/store";
 
 export default function DashboardPage() {
-  const { user, accessToken, refreshToken, authorization } = useSelector(
+  const { user } = useSelector(
     (state: RootState) => state.auth
   );
 
@@ -26,29 +26,7 @@ export default function DashboardPage() {
             </p>
           </div>
 
-          <div className="rounded-lg bg-gray-100 p-4">
-            <p className="text-sm text-gray-500">Access Token</p>
-
-            <p className="mt-1 break-all text-xs font-semibold text-gray-800">
-              {accessToken || "No access token found"}
-            </p>
-          </div>
-
-          <div className="rounded-lg bg-gray-100 p-4">
-            <p className="text-sm text-gray-500">Refresh Token</p>
-
-            <p className="mt-1 break-all text-xs font-semibold text-gray-800">
-              {refreshToken || "No refresh token found"}
-            </p>
-          </div>
-
-          <div className="rounded-lg bg-gray-100 p-4">
-            <p className="text-sm text-gray-500">Authorization Bearer Token</p>
-
-            <p className="mt-1 break-all text-xs font-semibold text-gray-800">
-              {authorization || "No authorization token found"}
-            </p>
-          </div>
+         
         </div>
       </div>
 
